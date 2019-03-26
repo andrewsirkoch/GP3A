8261ae28bd48181c84555d8b6a0af41b1703d0da
andrewsirkoch <sirkoa@rpi.edu>
2019-03-26 15:33:57 -0400

    v0.1 Base Game

---
+++ PROJECT/3A/Assets/PLAYER/Player_SetToOrigin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_SetToOrigin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Player").transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PROJECT/3A/Assets/Scenes/SceneManage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    private int timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += 1;
+        if (timer >= 200) Player_Info.ended = false;
+    }
+
+    public void Button_LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
